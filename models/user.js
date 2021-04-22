e0a168289ab0a95c9526280d6a2bf1e008c8d173
mcamarotte <76053716+mcamarotte@users.noreply.github.com>
2021-04-22 15:23:37 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require ('mongoose-unique-validator');
-
+// Squema validation for email and password
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
