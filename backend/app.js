820cba581f5cb8a483dc56915a88925dd5a32a53
mcamarotte <76053716+mcamarotte@users.noreply.github.com>
2021-04-27 11:44:52 +0100

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mongoSanitize = require('express-mongo-sanitize');
+
+const app = express();
+
+const path = require('path');
+
+require('dotenv').config();
+
+const saucesRoutes = require('./backend/routes/sauces');
+const userRoutes = require('./backend/routes/user')
+
+const Sauce = require('./backend/models/sauces');
+ 
+//Connect to the Mongo DB database
+mongoose
+  .connect(
+    `mongodb+srv://mcamarotte:<Ita218.Child.18*> @cluster0.q6ux1.mongodb.net/mcamarotte?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connection to MongoDB succesful!"))
+  .catch(() => console.log("Connection to MongoDB failure!"));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use(mongoSanitize({
+  replaceWith: '_'
+}))
+
+//make the images publicly accessible for all requests to the route - images
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes)
+
+module.exports = app;
