3ca247a4a7735e12cb10721e1d5257a77ea32b26
mcamarotte <76053716+mcamarotte@users.noreply.github.com>
2021-04-29 10:22:45 +0100

    Update sauces.js

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
-const saucesCtrl = require('../backend/controllers/sauces');
-
-const auth = require('../backend/middleware/auth');
-const multer = require('..backend/middleware/multer-config');
+const saucesCtrl = require('./controllers/sauces');
+const auth = require('./middleware/auth');
+const multer = require('./middleware/multer-config');
 
 router.post('/', auth, multer, saucesCtrl.createSauce);
 router.get('/:id', auth, saucesCtrl.getOneSauce);
