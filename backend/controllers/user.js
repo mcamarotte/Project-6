e597a7b6572f5c8e2ee2ef952ecc68a8771bb4d6
mcamarotte <76053716+mcamarotte@users.noreply.github.com>
2021-05-05 09:12:40 +0100

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,39 +1,27 @@
 const bcrypt = require('bcrypt');
 const webtoken = require('jsonwebtoken');
 const cryptojs = require('crypto-js');
-const User = require('./models/user');
+const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
+    //create hashes user password
+  bcrypt.hash(req.body.password, 10)      
+      .then(hash => {                     
+          const user = new User({         
+              email: req.body.email,
+              password: hash          
+          });
   
-  //require a strong password of at least 8 characters  
-  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z0-9\d@$!%*?&]{8,}$/; 
-  const password = req.body.password;
-  const crypEmail = cryptojs.HmacSHA256(req.body.email, process.env.EMAIL_ENCRYPTION_KEY).toString();
-
-  if (password.match(regex)) {
-    
-  //create hashes user password
-  bcrypt.hash(password, 10)
-    .then(hash => {
-      const user = new User({
-        email: crypEmail,
-        password: hash
-      });
-    
       //adds user to database
       user.save()
         .then(() => res.status(201).json({ message: 'User created!' }))
         .catch(error => res.status(400).json({ error }));
     })
     .catch(error => res.status(500).json({ error }));
-  } else {
-    throw new Error("Password is not secure enough");
-  }
 };
 
 exports.login = (req, res, next) => {
-  const crypEmail = cryptojs.HmacSHA256(req.body.email, process.env.EMAIL_ENCRYPTION_KEY).toString();
-  User.findOne({ email: crypEmail })
+  User.findOne({ email: req.body.email })
     .then(user => {
       if (!user) {
         return res.status(401).json({ error: 'User not found!' });
