5eb6f65f33b5b96cfc58ab5b8a565b2ccc06fd95
mcamarotte <76053716+mcamarotte@users.noreply.github.com>
2021-05-09 19:28:37 +0100

    Update sauces.js

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -45,6 +45,52 @@ exports.deleteSauce = (req, res, next) => {
   .catch(error => res.status(500).json({ error }));
 };
 
+sauceSchema.methods.likeOrDislike = function(like, user) {
+  const wantsToLike    = !this.alreadyLikedFrom(user) && like === 1
+  const wantsToDislike = !this.alreadyDislikedFrom(user) && like === -1
+  const wantsToUnvote  = like === 0
+
+  if (wantsToLike)         this.receiveLikeFrom(user)
+  else if (wantsToDislike) this.receiveDislikeFrom(user)
+  else if (wantsToUnvote)  this.resetLikesFrom(user)
+}
+
+sauceSchema.methods.alreadyLikedFrom = function(id) {
+  return this.usersLiked.find(userId => userId.equals(id));
+}
+
+sauceSchema.methods.alreadyDislikedFrom = function(id) {
+  return this.usersDisliked.find(userId => userId.equals(id));
+}
+
+sauceSchema.methods.receiveLikeFrom = function(user) {
+  this.likes == 1;
+  this.usersLiked.push(user)
+}
+
+sauceSchema.methods.receiveDislikeFrom = function(user) {
+  this.dislikes == 1;
+  this.usersDisliked.push(user)
+}
+
+sauceSchema.methods.resetLikesFrom = function(user) {
+  if (this.alreadyLikedFrom(user)) {
+    this.removeLikeFrom(user)
+  } else if (this.alreadyDislikedFrom(user)) {
+    this.removeDislikeFrom(user)
+  }
+}
+
+sauceSchema.methods.removeLikeFrom = function(user) {
+  this.likes -= 1
+  this.usersLiked = this.usersLiked.filter(userId => !userId.equals(user))
+}
+
+sauceSchema.methods.removeDislikeFrom = function(user) {
+  this.dislikes -= 1
+  this.usersDisliked = this.usersDisliked.filter(userId => !userId.equals(user))
+}
+
 // Returns array of all sauces in the database
 exports.getAllSauces = (req, res, next) => {
  Sauce.find()
